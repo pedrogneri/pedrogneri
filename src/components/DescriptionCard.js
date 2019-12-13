9b1ef536b2d0f3d450bd6aaf70c42abeda9096d2
Pedro Neri <pedrogneri@gmail.com>
2019-12-13 03:11:17 -0300

    Add descriptionCard component

---
+++ src/components/DescriptionCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function DescriptionCard(){
+    return (
+        <div className="w3-card-4">
+            <header className="w3-container w3-center primary-color">
+                <h1>Pedro Guilherme Neri</h1>
+            </header>
+                
+            <div className="w3-container secondary-color">
+                <p>Estagiário na Dextra e estudante de ciência da computação na UniFAJ</p>
+            </div>
+        </div>
+    )
+}
